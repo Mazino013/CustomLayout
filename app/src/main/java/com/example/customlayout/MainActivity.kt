b1c8b6db9632d5a355c98007692e7333bed86508
Zino <ewomazinoefe@gmail.com>
2024-09-02 20:05:00 -0400

    Clickable Functionality on inner circles

--- app/src/main/java/com/example/customlayout/MainActivity.kt
+++ app/src/main/java/com/example/customlayout/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.customlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
